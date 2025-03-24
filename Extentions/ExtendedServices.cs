b08b07ad91f3d0de9fdfed9d0bedaf2571e53630
Meleshko Danylo <dan20152004@gmail.com>
2025-03-24 18:25:06 +0100

    init

---
+++ Extentions/ExtendedServices.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using MyDrive.Data;
+using MyDrive.Models;
+
+namespace MyDrive.Extentions;
+
+public static class ExtendedServices
+{
+    public static IServiceCollection AddExtendedServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<AppDbContext>(opt =>
+            opt.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+        
+        services.AddIdentity<AppUser, IdentityRole>(opt =>
+        {
+            opt.Password.RequiredLength = 8;
+            opt.Password.RequireNonAlphanumeric = false;
+            opt.Password.RequireUppercase = false;
+            opt.Password.RequireLowercase = false;
+            opt.Password.RequireDigit = true;
+            
+            opt.User.RequireUniqueEmail = true;
+            opt.SignIn.RequireConfirmedAccount = false;
+            
+            opt.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(20);
+            opt.Lockout.MaxFailedAccessAttempts = 5;
+        })
+            .AddRoles<IdentityRole>()
+            .AddEntityFrameworkStores<AppDbContext>()
+            .AddDefaultTokenProviders()
+            .AddSignInManager();
+
+        services.AddDistributedMemoryCache();
+        services.AddSession(opt =>
+        {
+            opt.IdleTimeout = TimeSpan.FromDays(1);
+            opt.Cookie.HttpOnly = true;
+            opt.Cookie.IsEssential = true;
+            opt.Cookie.Name = "MyDrive.Session";
+            opt.Cookie.SameSite = SameSiteMode.Strict;
+            opt.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+        });
+        
+        services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+            .AddCookie(opt =>
+            {
+                opt.Cookie.Name = "MyDrive.Session";
+                opt.ExpireTimeSpan = TimeSpan.FromDays(14);
+            });
+        
+        return services;
+    }
+}
